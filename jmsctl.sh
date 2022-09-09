ea30f4ea0c0479735a5b93886b38ceeb7dac8940
Vladimir <vanpin@redsearch.ru>
2022-09-09 11:05:56 +0500

    поменял ссылку

--- jmsctl.sh
+++ jmsctl.sh
@@ -134,7 +134,7 @@ function check_update() {
   Install_DIR="$(cd "$(dirname "${PROJECT_DIR}")" >/dev/null 2>&1 && pwd)"
   if [[ ! -d "${Install_DIR}/jumpserver-installer-${latest_version}" ]]; then
     if [[ ! -f "${Install_DIR}/jumpserver-installer-${latest_version}.tar.gz" ]]; then
-      timeout 60s wget -qO "${Install_DIR}/jumpserver-installer-${latest_version}.tar.gz" "https://github.com/jumpserver/installer/releases/download/${latest_version}/jumpserver-installer-${latest_version}.tar.gz" || {
+      timeout 60s wget -qO "${Install_DIR}/jumpserver-installer-${latest_version}.tar.gz" "https://github.com/meowgen/installer/releases/download/${latest_version}/jumpserver-installer-${latest_version}.tar.gz" || {
         rm -f "${Install_DIR}/jumpserver-installer-${latest_version}.tar.gz"
         timeout 60s wget -qO "${Install_DIR}/jumpserver-installer-${latest_version}.tar.gz" "https://demo.jumpserver.org/download/installer/${latest_version}/jumpserver-installer-${latest_version}.tar.gz" || {
           rm -f "${Install_DIR}/jumpserver-installer-${latest_version}.tar.gz"
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -423,7 +423,7 @@ EOF
 }
 
 function get_latest_version() {
-  curl -s 'https://api.github.com/repos/jumpserver/jumpserver/releases/latest' |
+  curl -s 'https://api.github.com/repos/meowgen/jumpserver/releases/latest' |
     grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' |
     sed 's/\"//g;s/,//g;s/ //g'
 }
